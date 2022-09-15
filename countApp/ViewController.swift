4274397411d2c558a3353dfa1e2628bfde405d9f
Vasyl Larin <larinvasyl@protonmail.com>
2022-09-15 20:43:05 +0600

    Добавлен лейбл с выводом кол-ва нажатий на кнопку.

--- countApp/ViewController.swift
+++ countApp/ViewController.swift
@@ -8,12 +8,22 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
+    @IBOutlet weak var labelCount: UILabel!
+    @IBOutlet weak var buttonCount: UIButton!
+    var countClick = 0
+    
     override func viewDidLoad() {
+        labelCount.text = "Значение счётчика: \(countClick)"
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
-
-
+    
+    @IBAction func actionButtonCount(_ sender: Any) {
+        countClick += 1
+        labelCount.text = "Значение счётчика: \(countClick)"
+    }
+    
+    
+    
 }
-
